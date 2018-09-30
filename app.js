54f40f33541393bf7ca996079bf9555a500152db
cherrygao <qi.gao@sdbean.com>
2018-09-30 16:46:09 +0800

    引入cors模块处理跨域问题

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ var path = require('path');
 var ejs = require('ejs')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var goods = require('./routes/goods');
-
+var cors = require('cors')
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 // app.engine('.html', ejs.__express)
 app.set('view engine', 'jade');
+
+
+app.use(cors()); // 设置允许跨域
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -40,16 +41,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
-//设置跨域访问
-app.all('*', function(req, res, next) {    
-  res.header("Access-Control-Allow-Origin", "*");    
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");    
-  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");    
-  res.header("X-Powered-By",' 3.2.1')    
-  res.header("Content-Type", "application/json;charset=utf-8");    
-  next();
 });
 
 module.exports = app;
