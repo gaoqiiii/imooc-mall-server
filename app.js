06a2309b9d6db2c9816deb65da452299bf255f4f
cherrygao <qi.gao@sdbean.com>
2018-10-16 18:04:31 +0800

    【后端】分页排序

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var goods = require('./routes/goods');
+// 设置允许跨域
 var cors = require('cors')
 var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 // app.engine('.html', ejs.__express)
 app.set('view engine', 'jade');
+// app.set('view engine', 'html')
 
 
 app.use(cors()); // 设置允许跨域
--- routes/goods.js
+++ routes/goods.js
@@ -10,11 +10,28 @@ mongoose.connect('mongodb://127.0.0.1:27017/dumall')
 mongoose.connection.on('connected', function() {
   console.log('MongoDB connected success')
 })
-
+/*
+  /goods 获取上商品列表
+  @param [sort] 查询顺序 1->升序 -1->降序
+         [page] 页码
+         [pageSize] 每页容量
+*/
 router.get('/', function(req, res, next) {
+  // 获取前端传来的参数, req.param()取回的参数为string,需转成number
+  let sort = parseInt(req.param('sort'))
+  let page = parseInt(req.param('page'))
+  let pageSize = parseInt(req.param('pageSize'))
+  let skip = (page - 1) * pageSize // 跳过多少条数据
+  // 查询参数
+  let param = {}
+
+  let goodsModel = Goods.find(param).skip(skip).limit(pageSize)
+  goodsModel.sort({'salePrice': sort}) // 对salePrice字段进行排序
   // 参数1：查找条件 
   // 参数2：callback
-  Goods.find({}, function (err, doc) {
+
+  // Goods.find({}, function (err, doc) {
+  goodsModel.exec( function (err, doc) {
     if (err) {
       res.json({
         status: 1,
