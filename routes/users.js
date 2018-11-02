50ef445de918ced4fc1c9be3463788d841eaa95d
gaoqiiii <gaoqi0303@163.com>
2018-11-02 23:59:16 +0800

    [后端]实现登录、登出功能

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
-
+var User = require('../models/user')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// 登录
+router.post('/login', function(req, res, next){
+  var param = {
+    userName: req.body.userName,
+    userPwd: req.body.userPwd
+  }
+
+  User.findOne(param, function(err, doc) {
+    if (err) {
+      res.json({
+        status: '1',
+        msg: err.message
+      })
+    } else {
+      if (doc) {
+        res.cookie('userId', doc.userId, {
+          path: '/',
+          maxAge: 1000*60*60
+        })
+        res.json({
+          status: '0',
+          msg: 'success',
+          result: {
+            userName: doc.userName,     
+          }
+        })
+      }
+      
+    }
+  })
+});
+
+// 登出
+router.post('/logout', function(req, res, next){
+  res.cookie("userId","",{
+    path: "/",
+    maxAge: 0
+  })
+
+  res.json({
+    status: '0',
+    msg: '',
+    result: ''
+  })
+})
 module.exports = router;
